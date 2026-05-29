3ba3a2050baa6041a5cd509f4a7497f57f0e1d4d
sequoyametal-cloud <sequoya.metal@gmail.com>
2026-05-29 13:32:19 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "environmental-form-pwa-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./styles.css?v=1",
+    "./app.js?v=4",
+    "./db.js?v=6",
+    "./manifest.json",
+    "./logo-CMOC-Odin.png"
+];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(
+                keys.map(function(key) {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
